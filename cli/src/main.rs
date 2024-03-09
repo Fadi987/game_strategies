dc1be420cc7ce87fb433b83c08f3e945d4d92392
Fadi Atieh <fadi.atieh98@gmail.com>
2024-03-09 13:37:56 -0600

    Factor input parsing into its own function.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,6 +2,21 @@ use std::io;
 use tic_tac_toe::board;
 use tic_tac_toe::game;
 
+fn parse_input(input: &String) -> Result<(usize, usize), (&'static str)> {
+    let parts: Vec<&str> = input.split(',').collect();
+
+    if parts.len() != 2 {
+        return Err("Number of comma separated numbers must be 2.");
+    }
+
+    match (
+        parts[0].trim().parse::<usize>(),
+        parts[1].trim().parse::<usize>(),
+    ) {
+        (Ok(row_index), Ok(col_index)) => Ok((row_index, col_index)),
+        _ => Err("Must enter valid numbers separated by a comma."),
+    }
+}
 fn main() {
     let mut game = game::Game::new();
     loop {
@@ -18,7 +33,7 @@ fn main() {
         };
 
         println!(
-            "Select cell for player {} in format row_index,col_index: ",
+            "Select cell for player {} in format row_index, col_index: ",
             player
         );
 
@@ -27,31 +42,21 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read Tic-Tac-Toe move.");
 
-        let parts: Vec<&str> = input.split(',').collect();
-
-        if parts.len() == 2 {
-            match (parts[0].trim().parse::<usize>(), parts[1].trim().parse::<usize>()) {
-                (Ok(row_index), Ok(col_index)) => {
-
-                    match game.play(row_index, col_index) {
-                        Err(game::GamePlayError::MarkError(board::BoardMarkError::OutOfBound)) => {
-                            println!("Index out of bound. Try again.")
-                        },
-                        Err(game::GamePlayError::MarkError(board::BoardMarkError::NonEmptyCell)) => {
-                            println!("Cannot mark a non empty cell. Try again.")
-                        },
-                        Ok(()) => {
-                            continue;
-                        }
-                        _ => { 
-                            panic!("Should not get here!"); 
-                        }
-                    }
-                },
-                _ => println!("Invalid input. Please enter the row and column as numbers separated by a comma."),
+        if let Ok((row_index, col_index)) = parse_input(&input) {
+            match game.play(row_index, col_index) {
+                Err(game::GamePlayError::MarkError(board::BoardMarkError::OutOfBound)) => {
+                    println!("Index out of bound. Try again.")
+                }
+                Err(game::GamePlayError::MarkError(board::BoardMarkError::NonEmptyCell)) => {
+                    println!("Cannot mark a non empty cell. Try again.")
+                }
+                Ok(()) => {
+                    continue;
+                }
+                _ => {
+                    panic!("Should not get here!");
+                }
             }
-        } else {
-            println!("Invalid input format. Please use the format row_index,col_index.");
         }
     }
 }
